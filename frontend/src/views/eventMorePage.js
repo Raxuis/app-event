8a247bc90ed069cadf5377e3edb7c0c8c0a8ced9
Raxuis <raclot.raphael@gmail.com>
2024-05-29 22:07:13 +0200

    feat(event more page): :sparkles: add allocate and resources h3

--- frontend/src/views/eventMorePage.js
+++ frontend/src/views/eventMorePage.js
@@ -43,6 +43,29 @@ export default (event) => {
           </div>
         </div>
       </div>
+      </div>
+    <div class="relative flex-[1_auto] flex flex-col break-words min-w-0 bg-clip-border rounded-[.95rem] bg-white m-5">
+      <div class="relative flex flex-col min-w-0 break-words border border-dashed bg-clip-border rounded-2xl border-stone-200 bg-light/30">
+        <div class="p-6 flex justify-between">
+        <h3 class="flex items-center justify-center m-2 ml-0 font-medium text-xl/tight cursor-pointer text-black underline-offset-4 hover:underline gap-2">
+        Allocate resources.
+        <lord-icon 
+        class="size-7"
+        src="https://cdn.lordicon.com/iawrhwdo.json"
+        trigger="hover"
+        state="morph-coins">
+        </lord-icon>
+        </h3>
+        <h3 class="flex items-center justify-center m-2 ml-0 font-medium text-xl/tight cursor-pointer text-black underline-offset-4 hover:underline gap-2">
+        Check resources.
+        <lord-icon 
+        class="size-8"
+        src="https://cdn.lordicon.com/wyqtxzeh.json"
+        trigger="hover">
+        </lord-icon>
+        </h3>
+        </div>
+      </div>
     </div>
   </div>
 </div>

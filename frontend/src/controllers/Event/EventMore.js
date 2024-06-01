d959a69520ebed2cf8269a61cbc7ab3e6b947d5a
Raxuis <raclot.raphael@gmail.com>
2024-06-01 14:29:41 +0200

    fix(event more): :lipstick: fix issue with button go back always shown

--- frontend/src/controllers/Event/EventMore.js
+++ frontend/src/controllers/Event/EventMore.js
@@ -54,8 +54,8 @@ class EventMore {
       </div>
       <div class="w-full mx-auto flex flex-col items-center justify-center">
         ${viewEvent(this.response)}
-        <div class="flex-shrink-0 max-sm:flex w-full">
-          <button type="button" onclick="window.location.href='/my-events'" class="w-full flex items-center justify-center px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto hover:bg-gray-100">
+        <div class="flex-shrink-0 max-sm:flex w-full sm:hidden">
+          <button type="button" onclick="window.location.href='/my-events'" class="w-full sm:hidden flex items-center justify-center px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto hover:bg-gray-100">
             <svg class="w-5 h-5 rtl:rotate-180" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
             </svg>

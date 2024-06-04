3a3011ca2a43634f3ffea704697f1afe281384b5
Raxuis <raclot.raphael@gmail.com>
2024-06-04 10:23:05 +0200

    feat(social media): :sparkles: add button to share to social medias

--- frontend/src/views/myEvents/event.js
+++ frontend/src/views/myEvents/event.js
@@ -46,6 +46,27 @@ export default (event, userId, specificGuests) => {
           <div class="bg-yellow-200 text-yellow-700 py-1.5 px-6 rounded-full hover:bg-yellow-300 duration-300 cursor-default">
             <p tabindex="0" class="focus:outline-none text-xs">By ${userId === event.author_id ? 'Yourself' : `${event.author_lastname[0]}.${event.author_firstname}`}</p>
           </div>
+  <div class="relative inline-block text-left dropdown">
+    <span class="rounded-md shadow-sm"
+      ><button class="inline-flex justify-center w-full px-4 py-2 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800" 
+       type="button" aria-haspopup="true" aria-expanded="true" aria-controls="headlessui-menu-items-117">
+        <span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-share-2 size-4 mt-0.5"><circle cx="18" cy="5" r="3"/><circle cx="6" cy="12" r="3"/><circle cx="18" cy="19" r="3"/><line x1="8.59" x2="15.42" y1="13.51" y2="17.49"/><line x1="15.41" x2="8.59" y1="6.51" y2="10.49"/></svg></span>
+        <svg class="w-5 h-5 ml-2 -mr-1" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+        </button
+    ></span>
+    <div class="opacity-0 invisible dropdown-menu transition-all duration-300 transform origin-top-right -translate-y-2 scale-95">
+      <div class="absolute right-0 w-56 mt-2 origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg outline-none" aria-labelledby="headlessui-menu-button-1" id="headlessui-menu-items-117" role="menu">
+        <div class="px-4 py-3">         
+          <p class="text-sm leading-5">Share to Social Medias</p>
+        </div>
+        <div class="py-1">
+          <a href="#" tabindex="0" class="text-gray-700 flex justify-between w-full px-4 py-2 text-sm leading-5 text-left" role="menuitem">Facebook</a>
+          <a href="#" tabindex="1" class="text-gray-700 flex justify-between w-full px-4 py-2 text-sm leading-5 text-left" role="menuitem">Twitter / X</a>
+          <span role="menuitem" tabindex="-1" class="flex justify-between w-full px-4 py-2 text-sm leading-5 text-left text-gray-700 cursor-not-allowed opacity-50" aria-disabled="true">TikTok (soon)</span>
+          <a href="#" tabindex="2" class="text-gray-700 flex justify-between w-full px-4 py-2 text-sm leading-5 text-left" role="menuitem">Instagram</a></div>
+      </div>
+    </div>
+  </div>        
         </div>
         <div class="p-4 flex flex-col flex-1">
           <div class="flex items-center">

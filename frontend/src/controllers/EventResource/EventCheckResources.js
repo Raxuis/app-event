a89231e6f4ef4005ba16d5f91641819fdc51ec29
Raxuis <raclot.raphael@gmail.com>
2024-06-10 14:09:38 +0200

    style(go back buttons): :lipstick: stylized go back buttons
    stylized go back buttons to make them respect responsive and full large screen

--- frontend/src/controllers/EventResource/EventCheckResources.js
+++ frontend/src/controllers/EventResource/EventCheckResources.js
@@ -117,9 +117,9 @@ class EventCheckResources {
     return `
     ${viewNav(this.userId)}
     <div class="container mx-auto h-screen p-6 mt-4">
-      <div class="flex flex-wrap gap-4">
+    <div class="flex flex-wrap gap-4 max-sm:hidden">
         <div class="flex-shrink-0">
-          <button type="button" class="go-back-check flex items-center justifycenter px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto hover:bg-gray-100">
+          <button type="button" onclick="window.location.href='/my-events'" class="flex items-center justify-center px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto hover:bg-gray-100">
             <svg class="w-5 h-5 rtl:rotate-180" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
             </svg>
@@ -128,7 +128,15 @@ class EventCheckResources {
         </div>
       </div>
       <div class="w-full mx-auto flex flex-col items-center justify-center">
-        ${viewCheckRessources(this.eventInfos, this.response)}
+      ${viewCheckRessources(this.eventInfos, this.response)}
+        <div class="flex-shrink-0 max-sm:flex w-full sm:hidden m-4">
+          <button type="button" onclick="window.location.href='/my-events'" class="w-full sm:hidden flex items-center justify-center px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto hover:bg-gray-100">
+            <svg class="w-5 h-5 rtl:rotate-180" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+            </svg>
+            <span>Go back</span>
+          </button>
+        </div>
       </div>
     </div>
     `;

b677fdf682c7fe962313e273779eb1103607d03c
Raxuis <raclot.raphael@gmail.com>
2024-06-10 13:59:44 +0200

    fix(event check resources): :money_with_wings: moved the dollar sign before the number

--- frontend/src/views/eventResource/eventCheckResource/eventCheckTable/eventCheckCol.js
+++ frontend/src/views/eventResource/eventCheckResource/eventCheckTable/eventCheckCol.js
@@ -17,7 +17,7 @@ export default (resource) => {
   <span class="text-center align-baseline inline-flex px-2 py-1 mr-auto items-center font-semibold text-base/none rounded-lg">${type[0].toUpperCase() + type.slice(1)}</span>
   </td>
   <td class="p-3 text-center">
-  <span class="font-semibold text-light-inverse text-md/normal">${cost}$</span>
+  <span class="font-semibold text-light-inverse text-md/normal">$${cost}</span>
   </td>
   <td class="flex justify-center items-center gap-3">
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-minus h-6 w-5 cursor-pointer decrement-${eventResourceId}"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l14 0" /></svg>

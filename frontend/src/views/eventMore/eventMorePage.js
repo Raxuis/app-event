a1cb2b2c0abf482d5ad66f8ae706b9bf2b40549e
Raxuis <raclot.raphael@gmail.com>
2024-06-11 11:19:33 +0200

    style(resources buttons): :lipstick: stylized resources buttons to make them more understandable and visible

--- frontend/src/views/eventMore/eventMorePage.js
+++ frontend/src/views/eventMore/eventMorePage.js
@@ -27,16 +27,16 @@ export default (event, userId) => {
       <div class="relative flex-[1_auto] flex flex-col break-words min-w-0 bg-clip-border rounded-[.95rem] bg-white m-5">
         <div class="relative flex flex-col min-w-0 break-words border border-dashed bg-clip-border rounded-2xl border-stone-200 bg-light/30">
           <div class="p-6 flex flex-col">
-            <h3 class="flex items-center m-2 ml-0 font-medium text-2xl/tight text-black-900 gap-2">Resources.</h3>
-            <div class="w-full flex justify-between max-sm:flex-col">
-              <a class="allocate-resources flex items-center m-2 ml-0 font-medium text-lg/tight cursor-pointer text-black-900 underline-offset-4 hover:underline gap-2">
-                Allocate resource.
-                <lord-icon class="size-6" src="https://cdn.lordicon.com/iawrhwdo.json" trigger="hover" state="morph-coins"></lord-icon>
+            <h3 class="flex items-center m-2 ml-0 font-medium text-2xl/tight text-black-900 gap-2">Resources</h3>
+            <div class="w-full flex justify-between max-sm:flex-col max-sm:space-y-2">
+              <a class="allocate-resources inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input bg-transparent shadow-sm hover:bg-electric-violet-200 h-9 px-4 py-2 gap-2 duration-300">
+                Allocate resource
+                <lord-icon class="size-5" src="https://cdn.lordicon.com/iawrhwdo.json" trigger="hover" state="morph-coins"></lord-icon>
               </a>
               ${eventResources.length > 0 ? `
-                <a class="check-resources flex items-center m-2 ml-0 font-medium text-lg/tight cursor-pointer text-black-900 underline-offset-4 hover:underline gap-2">
-                  Check resources.
-                  <lord-icon class="size-6" src="https://cdn.lordicon.com/wyqtxzeh.json" trigger="hover"></lord-icon>
+                <a class="check-resources inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input bg-transparent shadow-sm hover:bg-electric-violet-200 h-9 px-4 py-2 gap-2 duration-300">
+                  Check resources
+                  <lord-icon class="size-5" src="https://cdn.lordicon.com/wyqtxzeh.json" trigger="hover"></lord-icon>
                 </a>` : `
                 <h3 class="flex items-center m-2 ml-0 font-medium text-lg/tight text-black-900 cursor-default gap-2">
                   You have no resource, please allocate one first.

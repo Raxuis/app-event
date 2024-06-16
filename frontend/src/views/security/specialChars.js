eaea34ce6e8947aaa937c56a2c7bbb02bb46c4d5
Raxuis <raclot.raphael@gmail.com>
2024-06-16 16:27:37 +0200

    fix(specialChars function): :bug: fix issue when param is null

--- frontend/src/views/security/specialChars.js
+++ frontend/src/views/security/specialChars.js
@@ -1,6 +1,11 @@
 import he from 'he';
 
 function convertEntitiesHTML(text) {
+  // Checking if text is null or undefined
+  if (text === null || typeof text === 'undefined') {
+    return ''; // Returning an empty string if value is null or undefined
+  }
+
   // Decoding HTML entities using library he
   let decodedText = he.decode(text);
 
